e6f336ebfc5db8f670718ff4bae4b1b463882201
RoyCvt <royes.cvt@gmail.com>
2024-10-08 16:07:11 +0300

    added a returned feedback message that explains why registration failed when it does

--- image_registration.py
+++ image_registration.py
@@ -144,58 +144,92 @@ def find_biggest_bounded_rect(image):
 
 
 def register_images(base64_image1, base64_image2):
-    # Decode the base64 images to numpy arrays
-    image1 = decode_image(base64_image1)
-    image2 = decode_image(base64_image2)
+    """
+    Performs registration between 2 images.
+    Params:
+        base64_image1: The first image encoded to a string with base64.
+        base64_image2: The second image encoded to a string with base64.
+    Returns:
+        tuple:
+            If registration is successful:
+                (True, first registered image encoded to base64 string, second registered image encoded to base64 string)
+            If registration fails:
+                (False, failure reason, status code)
+    """
+    if not isinstance(base64_image1, str) or not isinstance(base64_image2, str):
+        return (False, 'Bad input', 400)
 
-    # Get the dimensions of the images
-    image1_height, image1_width = image1.shape[:2]
-    image2_height, image2_width = image2.shape[:2]
+    action = ""
+    try:
+        # Decode the images from base64 strings to numpy arrays
+        action = 'decoding the images from base64 strings to numpy arrays'
+        image1 = decode_image(base64_image1)
+        image2 = decode_image(base64_image2)
 
-    # Set scaling factors for downscaling the images to improve performance
-    # Ensures that images are downscaled as much as possible while keeping their smaller dimension above 1000
-    scaling_factor1 = min(image1_height, image1_width) / 1000 if min(image1_height, image1_width) > 1000 else 1
-    scaling_factor2 = min(image2_height, image2_width) / 1000 if min(image2_height, image2_width) > 1000 else 1
+        # Get the dimensions of the images
+        action = 'getting the height and width of the images'
+        image1_height, image1_width = image1.shape[:2]
+        image2_height, image2_width = image2.shape[:2]
 
-    # Calculate the new dimensions for both images based on their scaling factors
-    downscaled_image1_width = int(image1_width / scaling_factor1)
-    downscaled_image1_height = int(image1_height / scaling_factor1)
-    downscaled_image2_width = int(image2_width / scaling_factor2)
-    downscaled_image2_height = int(image2_height / scaling_factor2)
+        # Set scaling factors for downscaling the images to improve performance
+        # Ensures that images are downscaled as much as possible while keeping their smaller dimension above 1000
+        action = 'calculating the optimal scaling factor for each of the images'
+        scaling_factor1 = min(image1_height, image1_width) / 1000 if min(image1_height, image1_width) > 1000 else 1
+        scaling_factor2 = min(image2_height, image2_width) / 1000 if min(image2_height, image2_width) > 1000 else 1
 
-    # Downscale the images to make feature extraction and feature matching faster and reduce memory usage
-    downscaled_image1 = cv2.resize(image1, (downscaled_image1_width, downscaled_image1_height))
-    downscaled_image2 = cv2.resize(image2, (downscaled_image2_width, downscaled_image2_height))
+        # Calculate the new dimensions for both images based on their scaling factors
+        action = 'calculating the width and height of the downscaled versions of the images'
+        downscaled_image1_width = int(image1_width / scaling_factor1)
+        downscaled_image1_height = int(image1_height / scaling_factor1)
+        downscaled_image2_width = int(image2_width / scaling_factor2)
+        downscaled_image2_height = int(image2_height / scaling_factor2)
 
-    # Extract keypoints and descriptors from the downscaled images
-    downscaled_keypoints1, downscaled_descriptors1 = extract_keypoints_and_descriptors(downscaled_image1)
-    downscaled_keypoints2, downscaled_descriptors2 = extract_keypoints_and_descriptors(downscaled_image2)
+        # Downscale the images to make feature extraction and feature matching faster and reduce memory usage
+        action = 'downscaling the images'
+        downscaled_image1 = cv2.resize(image1, (downscaled_image1_width, downscaled_image1_height))
+        downscaled_image2 = cv2.resize(image2, (downscaled_image2_width, downscaled_image2_height))
 
-    # Find matches between the descriptors of the downscaled images using FLANN
-    matches = find_matches(downscaled_descriptors1, downscaled_descriptors2)
+        # Extract keypoints and descriptors from the downscaled images
+        action = "extracting keypoints and descriptors from the images"
+        downscaled_keypoints1, downscaled_descriptors1 = extract_keypoints_and_descriptors(downscaled_image1)
+        downscaled_keypoints2, downscaled_descriptors2 = extract_keypoints_and_descriptors(downscaled_image2)
 
-    # Get the strong matches based on Lowe's ratio test
-    strong_matches = get_strong_matches(matches)
+        # Find matches between the descriptors of the downscaled images using FLANN
+        action = "searching for matches between the images"
+        matches = find_matches(downscaled_descriptors1, downscaled_descriptors2)
 
-    # Upscale the keypoints' coordinates back to match the original image sizes
-    keypoints1 = [cv2.KeyPoint(scaling_factor1 * keypoint.pt[0], scaling_factor1 * keypoint.pt[1], 1) for keypoint in downscaled_keypoints1]
-    keypoints2 = [cv2.KeyPoint(scaling_factor2 * keypoint.pt[0], scaling_factor2 * keypoint.pt[1], 1) for keypoint in downscaled_keypoints2]
+        # Get the strong matches based on Lowe's ratio test
+        action = "removing weak matches"
+        strong_matches = get_strong_matches(matches)
 
-    # Compute the homography matrix that aligns the first image to the second image's perspective
-    homography_matrix = compute_homography(strong_matches, keypoints1, keypoints2)
+        # Upscale the keypoints' coordinates back to match the original image sizes
+        action = "upsacling the coordinates of the matches' keypoints"
+        keypoints1 = [cv2.KeyPoint(scaling_factor1 * keypoint.pt[0], scaling_factor1 * keypoint.pt[1], 1) for keypoint in downscaled_keypoints1]
+        keypoints2 = [cv2.KeyPoint(scaling_factor2 * keypoint.pt[0], scaling_factor2 * keypoint.pt[1], 1) for keypoint in downscaled_keypoints2]
 
-    # Warp the first image using the computed homography to match the second image's perspective
-    warped_image1 = cv2.warpPerspective(image1, homography_matrix, (image2_width, image2_height))
+        # Compute the homography matrix that aligns the first image to the second image's perspective
+        action = "computing the homography matrix"
+        homography_matrix = compute_homography(strong_matches, keypoints1, keypoints2)
 
-    # Find the biggest rectangular region in the warped first image that doesn't contain any padding (black areas)
-    top_left, bottom_right = find_biggest_bounded_rect(warped_image1)
+        # Warp the first image using the computed homography to match the second image's perspective
+        action = "warping the perspective of the first image based on the homography matrix"
+        warped_image1 = cv2.warpPerspective(image1, homography_matrix, (image2_width, image2_height))
 
-    # Crop the common region between the two images to prepare for overlay
-    cropped_warped_image1 = warped_image1[top_left[1] : bottom_right[1], top_left[0] : bottom_right[0]]
-    cropped_image2 = image2[top_left[1] : bottom_right[1], top_left[0] : bottom_right[0]]
+        # Find the biggest rectangular region in the warped first image that doesn't contain any padding (black areas)
+        action = "finding the largest area without padding in the first image"
+        top_left, bottom_right = find_biggest_bounded_rect(warped_image1)
 
-    # Encode the images to base64
-    base64_registered_image1 = encode_image(cropped_warped_image1)
-    base64_registered_image2 = encode_image(cropped_image2)
+        # Crop the common region between the two images to prepare for overlay
+        action = "cropping the images in the same places so they become registered and without padding"
+        cropped_warped_image1 = warped_image1[top_left[1] : bottom_right[1], top_left[0] : bottom_right[0]]
+        cropped_image2 = image2[top_left[1] : bottom_right[1], top_left[0] : bottom_right[0]]
 
-    return base64_registered_image1, base64_registered_image2
+        # Encode the images to base64
+        action = "encoding the registration images from numpy arrays to base64 strings"
+        base64_registered_image1 = encode_image(cropped_warped_image1)
+        base64_registered_image2 = encode_image(cropped_image2)
+
+        return (True, base64_registered_image1, base64_registered_image2)
+
+    except Exception:
+        return (False, f"Encountered unexpected error while {action}", 500)
