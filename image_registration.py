d8d2586ac1ba9c644452ab93052d1f05f8a4f41b
RoyCvt <royes.cvt@gmail.com>
2024-11-12 14:43:28 +0200

    removed redundant function "find_smallest_bounding_rect", documented the code further, and removed performance enhancing measure that hurts the quality of results

--- image_registration.py
+++ image_registration.py
@@ -68,23 +68,6 @@ def compute_homography(matches, keypoints1, keypoints2):
     return homography_matrix
 
 
-def find_smallest_bounding_rect(image):
-    """Finds the Top-Left and Bottom-Right Coordinates of the Smallest Rectangle that Can Bound the non-black Pixels of the Image"""
-    # Convert the color-space of the image from RGB to Grayscale
-    image_gray = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
-    # Create a binary mask of the image by setting all non-black pixels to 255, and black (padding) to 0
-    _, image_mask = cv2.threshold(src=image_gray, thresh=0, maxval=255, type=cv2.THRESH_BINARY)
-    # Find the contours of the mask
-    contours, _ = cv2.findContours(image=image_mask, mode=cv2.RETR_EXTERNAL, method=cv2.CHAIN_APPROX_SIMPLE)
-    # Keep the biggest contour of the mask (if for some reason it has more than one contour)
-    biggest_contour = sorted(list(contours), key=cv2.contourArea, reverse=True)[0]
-    # Find the top-left point of the bounding rectangle
-    top_left = np.squeeze(biggest_contour.min(axis=0)).tolist()
-    # Find the bottom-right point of the bounding rectangle
-    bottom_right = np.squeeze(biggest_contour.max(axis=0)).tolist()
-    return top_left, bottom_right
-
-
 def refine_contour(contour, epsilon_factor=0.01):
     """
     Refines the contour by reducing the number of points using contour approximation.
@@ -169,14 +152,14 @@ def find_biggest_bounded_rect(image):
 
 def register_images(base64_image1, base64_image2):
     """
-    Performs registration between 2 images.
+    Registers two images by aligning them based on matched keypoints and descriptors.
     Params:
-        base64_image1: The first image encoded to a string with base64.
-        base64_image2: The second image encoded to a string with base64.
+        base64_image1: The first image encoded to a base64 string.
+        base64_image2: The second image encoded to a base64 string.
     Returns:
         tuple:
             If registration is successful:
-                (True, first registered image encoded to base64 string, second registered image encoded to base64 string)
+                (True, base64 string of first registered image, base64 string of second registered image)
             If registration fails:
                 (False, failure reason, status code)
     """
@@ -195,43 +178,35 @@ def register_images(base64_image1, base64_image2):
         image1_height, image1_width = image1.shape[:2]
         image2_height, image2_width = image2.shape[:2]
 
-        for min_size in range(200, 1001, 200):
-            # Set scaling factors for downscaling the images to improve performance
-            # Ensures that images are downscaled as much as possible while keeping their smaller dimension above a minimum size
-            action = "חישוב מקדם הקטנה מיטבי לכל תמונה"
-            scaling_factor1 = min(image1_height, image1_width) / min_size if min(image1_height, image1_width) > min_size else 1
-            scaling_factor2 = min(image2_height, image2_width) / min_size if min(image2_height, image2_width) > min_size else 1
+        # Set scaling factors for downscaling the images to improve performance
+        action = "חישוב מקדם הקטנה לכל תמונה"
+        scaling_factor1 = min(image1_height, image1_width) / 1000 if min(image1_height, image1_width) > 1000 else 1
+        scaling_factor2 = min(image2_height, image2_width) / 1000 if min(image2_height, image2_width) > 1000 else 1
 
-            # Calculate the new dimensions for both images based on their scaling factors
-            action = "חישוב אורך ורוחב הגרסאות המוקטנות של התמונות"
-            downscaled_image1_width = int(image1_width / scaling_factor1)
-            downscaled_image1_height = int(image1_height / scaling_factor1)
-            downscaled_image2_width = int(image2_width / scaling_factor2)
-            downscaled_image2_height = int(image2_height / scaling_factor2)
+        # Calculate the new dimensions for both images based on their scaling factors
+        action = "חישוב אורך ורוחב הגרסאות המוקטנות של התמונות"
+        downscaled_image1_width = int(image1_width / scaling_factor1)
+        downscaled_image1_height = int(image1_height / scaling_factor1)
+        downscaled_image2_width = int(image2_width / scaling_factor2)
+        downscaled_image2_height = int(image2_height / scaling_factor2)
 
-            # Downscale the images to make feature extraction and feature matching faster and reduce memory usage
-            action = "הקטנת התמונות"
-            downscaled_image1 = cv2.resize(image1, (downscaled_image1_width, downscaled_image1_height))
-            downscaled_image2 = cv2.resize(image2, (downscaled_image2_width, downscaled_image2_height))
+        # Downscale the images to make feature extraction and feature matching faster and reduce memory usage
+        action = "הקטנת התמונות"
+        downscaled_image1 = cv2.resize(image1, (downscaled_image1_width, downscaled_image1_height))
+        downscaled_image2 = cv2.resize(image2, (downscaled_image2_width, downscaled_image2_height))
 
-            # Extract keypoints and descriptors from the downscaled images
-            action = "חילוץ נקודות חשובות מהתמונות"
-            downscaled_keypoints1, downscaled_descriptors1 = extract_keypoints_and_descriptors(downscaled_image1)
-            downscaled_keypoints2, downscaled_descriptors2 = extract_keypoints_and_descriptors(downscaled_image2)
+        # Extract keypoints and descriptors from the downscaled images
+        action = "חילוץ נקודות חשובות מהתמונות"
+        downscaled_keypoints1, downscaled_descriptors1 = extract_keypoints_and_descriptors(downscaled_image1)
+        downscaled_keypoints2, downscaled_descriptors2 = extract_keypoints_and_descriptors(downscaled_image2)
 
-            # Find matches between the descriptors of the downscaled images using brute force
-            action = "חיפוש התאמות בין התמונות"
-            matches = find_matches(downscaled_descriptors1, downscaled_descriptors2)
+        # Find matches between the descriptors of the downscaled images using brute force
+        action = "חיפוש התאמות בין התמונות"
+        matches = find_matches(downscaled_descriptors1, downscaled_descriptors2)
 
-            # Get the strong matches based on Lowe's ratio test
-            action = "הסרת התאמות חלשות"
-            strong_matches = get_strong_matches(matches)
-
-            # Check if the amount of strong matches between the images is enough for good registration
-            action = "בדיקת כמות ההתאמות הטובות בין התמונות"
-            if len(strong_matches) >= 25:
-                # No need to increase the image sizes for more strong matches
-                break
+        # Get the strong matches based on Lowe's ratio test
+        action = "הסרת התאמות חלשות"
+        strong_matches = get_strong_matches(matches)
 
         # Check if the amount of strong matches between the images is not enough for good registration
         action = "בדיקת כמות ההתאמות הטובות בין התמונות"
@@ -304,31 +279,49 @@ def register_images(base64_image1, base64_image2):
 
 
 def main():
+    # Reading the images
     image1 = plt.imread("images/1.jpg")
     image2 = plt.imread("images/2.jpg")
 
+    # Encoding the images to base64 strings
     encoded_image1 = encode_image(image1)
     encoded_image2 = encode_image(image2)
 
+    # Performing registration between the images
     registration_result = register_images(encoded_image1, encoded_image2)
 
+    # Get the status (success or failure) of the registration operation
     is_registration_successful = registration_result[0]
 
+    # Check if the registration was successful
     if is_registration_successful:
+        # Get the base64 encoded registration images
         base64_registered_image1 = registration_result[1]
         base64_registered_image2 = registration_result[2]
 
-        # Decode the base64 representation of the image
+        # Decode the base64 representation of the registration images
         registered_image1 = decode_image(base64_registered_image1)
         registered_image2 = decode_image(base64_registered_image2)
 
-        plt.subplot(121)
+        # Display the registration result
+        plt.figure(figsize=(12, 8))
+        plt.subplot(221)
+        plt.title("First image")
+        plt.imshow(image1)
+        plt.subplot(222)
+        plt.title("Second image")
+        plt.imshow(image2)
+        plt.subplot(223)
+        plt.title("Registered first image")
         plt.imshow(registered_image1)
-        plt.subplot(122)
+        plt.subplot(224)
+        plt.title("Registered second image")
         plt.imshow(registered_image2)
+        plt.tight_layout()
         plt.show()
 
     else:
+        # Get the reason that caused the registration to fail
         registration_failure_reason = registration_result[1]
         print('Registration failed!')
         print(registration_failure_reason)
@@ -336,4 +329,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-    
